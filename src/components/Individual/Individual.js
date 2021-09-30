3ae68c2a6939cde3a456aed74ca084db7020c8f9
tanishatasin <tasnintanisha@gmail.com>
2021-09-30 21:12:21 +0600

    add more

---
+++ src/components/Individual/Individual.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Individual = () => {
+               return (
+                              <div>
+                                    <h1>my individual</h1>         
+                              </div>
+               );
+};
+
+export default Individual;
